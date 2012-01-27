c69015ef4bc427b519c82e1870cabbdca82f7477
Marcelo Gornstein <marcelog@gmail.com>
2012-01-27 10:59:02 -0300

    removed some unneeded code

--- bin/bootstrap.php
+++ bin/bootstrap.php
@@ -1,20 +1,8 @@
 <?php
 $myDir = realpath(__DIR__ . '/..');
-define('ROOT_APPLICATION_PATH', $myDir);
-
-$confDir = $myDir . '/config';
-$vendorsDir = $myDir . '/vendor';
 $srcDir = $myDir . '/src/php';
 
-// Do your thing needed to bootstrap your app
-ini_set('include_path', implode(PATH_SEPARATOR, array(
-    $srcDir,
-    $vendorsDir . '/php',
-    $vendorsDir . '/php/Doctrine',
-    $vendorsDir . '/php/log4php',
-    ini_get('include_path')
-)));
-
+// Register a cute autoloader for our own files.
 spl_autoload_register(function($class) use ($srcDir) {
     $file = "$srcDir/$class.php";
     if (file_exists($file)) {
