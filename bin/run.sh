e681d0a019a182cd3b2685107afac2cc069e71fd
Marcelo Gornstein <marcelog@gmail.com>
2012-05-05 08:40:36 -0300

    replaced gnu readlink by a pure bash solution

--- bin/run.sh
+++ bin/run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-me=$(dirname ${0})
-root=$(readlink -f ${me}/..)
+
+# Get our base directory (the one where this script is located)
+# http://mywiki.wooledge.org/BashFAQ/028
+me=$([[ $0 == /* ]] && echo "$0" || echo "${PWD}/${0#./}")
+root=$(dirname ${me})/..
 . ${root}/config/cli.properties
 
 # The first argument is the php script to run (the stub). 
